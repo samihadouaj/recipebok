198610ce99f3a6467e1d181c1aeaef36aaec221e
sami <samihadouaj@gmail.com>
2018-07-16 21:28:11 +0100

    correction 404

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// app.get('/*', function(req,res) {
+//
+//   res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// });
+app.get('*', function (req, res) {
+  res.sendfile('./dist/index.html'); // load our index.html file
 });
 
 // Start the app by listening on the default Heroku port
